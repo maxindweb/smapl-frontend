2a3dbffc4c506383e6a9c6c09e70ff6941fd8b38
hamonangan silalahi <yohaneshamonangan@ndamelweb.id>
2020-12-02 09:32:16 +0700

    add login&media page

---
+++ pages/admin/media.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <v-container>
+            <h1>Media Page</h1>
+        </v-container>
+    </div>
+</template>
+<script>
+export default {
+    layout: 'admin'
+}
+</script>
