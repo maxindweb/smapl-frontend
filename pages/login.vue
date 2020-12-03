c2b9e082f6703f7938fd3edf284d1bedbbf3698f
hamonangan silalahi <yohaneshamonangan@ndamelweb.id>
2020-12-03 08:59:17 +0700

    create login function

--- pages/login.vue
+++ pages/login.vue
@@ -1,10 +1,50 @@
 <template>
     <div>
-        
+        <v-container>
+            <v-row justify-lg="center" align="end">
+                <v-col cols="6">
+                    <v-card>
+                        <v-form>
+                            <template>
+                                <v-container>
+                                    <v-text-field
+                                    v-model="email"
+                                    label="email"
+                                    ></v-text-field>
+                                    <v-text-field
+                                    v-model="password"
+                                    label="password"
+                                    ></v-text-field>
+                                </v-container>
+                            </template>
+                            <v-row>
+                                <v-col>
+                                    <v-btn
+                                    type="submit"
+                                    color="info"
+                                    > Login</v-btn>
+                                </v-col>
+                            </v-row>
+                        </v-form>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 <script>
 export default {
-    
+    data: () => ({
+      drawer: null,
+      loading: false,
+      email: undefined,
+      password: undefined,
+      
+}),
+    methods: {
+        async login(){
+            var res = $axios.post('/auth/login')
+        }
+    },
 }
 </script>
